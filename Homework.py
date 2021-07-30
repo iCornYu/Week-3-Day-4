0f1e1d73696e2ad031a5f3859e1ce543110df389
iCornYu <cookie3bar@gmail.com>
2021-07-30 14:57:48 -0700

    final

--- Homework.py
+++ Homework.py
@@ -6,9 +6,9 @@ file2.close
 homeworkpattern = re.compile('(?P<first_name>[A-Z][a-z]+) (?P<last_name>[\w]+) ?(?P<last_name2>[\w]*)')
 for line in data2:
     if homeworkpattern.match(line):
-        print(line)
+        print(line.strip("\n"))
     else:
         print(None)
 
 
-#could not get a for loop to work through the txt.file. It kept returning letters as the iterables instead of phrases.
+
