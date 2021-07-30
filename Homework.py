8963778e64430f2c374b746f12dae2639f08317b
iCornYu <cookie3bar@gmail.com>
2021-07-30 14:56:54 -0700

    edit2

--- Homework.py
+++ Homework.py
@@ -1,6 +1,6 @@
 import re
 file2 = open('Regex.txt')
-data2 = [file2.read()]
+data2 = [file2.readlines()]
 print(data2)
 file2.close
 homeworkpattern = re.compile('(?P<first_name>[A-Z][a-z]+) (?P<last_name>[\w]+) ?(?P<last_name2>[\w]*)')
