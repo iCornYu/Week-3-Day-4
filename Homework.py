c998b6e3bf2ee52059b514183e08045166ba467b
iCornYu <cookie3bar@gmail.com>
2021-07-30 14:56:06 -0700

    edit

--- Homework.py
+++ Homework.py
@@ -4,11 +4,9 @@ data2 = [file2.read()]
 print(data2)
 file2.close
 homeworkpattern = re.compile('(?P<first_name>[A-Z][a-z]+) (?P<last_name>[\w]+) ?(?P<last_name2>[\w]*)')
-matched = homeworkpattern.finditer(data2)
-print(homeworkpattern.findall(data2))
-for line in matched:
-    if line.group('last_name'):
-        print(f"{line.group('first_name')} {line.group('last_name')} {line.group('last_name2')}")
+for line in data2:
+    if homeworkpattern.match(line):
+        print(line)
     else:
         print(None)
 
